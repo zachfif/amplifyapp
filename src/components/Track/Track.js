ce27c3fa77eb25831a11e08f4ef8807cb2d7f9ce
zachery pfeifer <zachpfeifer13@gmail.com>
2022-09-06 13:11:38 -0400

    initial commit

---
+++ src/components/Track/Track.js
@@ -0,0 +1,20 @@
+
+import React from 'react'
+import './Track.css'
+
+function Track(props) {
+    return (
+        <div className="Track">
+            <div className="Track-information">
+                <h3>{props.track.title}</h3>
+                <p>{props.track.artist} | {props.track.album}</p>
+            </div>
+            <a className="Track-action" 
+            onClick={() => props.handleTrackAction(props.track)}>
+                {props.trackActionCharacter}
+            </a>
+        </div>
+    )
+}
+
+export default Track;
